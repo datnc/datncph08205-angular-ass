40633995920fab3fcfa5c455a304cedfc34470ce
datnc <hipboylc@gmail.com>
2020-04-17 23:05:56 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AdminComponent } from './backend/admin/admin.component';
@@ -9,7 +9,7 @@ import { DashboardComponent } from './backend/dashboard/dashboard.component';
 import { HomeComponent } from './frontend/home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, AdminComponent, DashboardComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
